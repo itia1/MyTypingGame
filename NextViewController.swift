39ebaff707759b424f1c01f39c7fe42590333699
itia1 <itia0503@gmail.com>
2015-10-02 01:14:22 +0900

    // add NextViewController

---
+++ NextViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NextViewController.swift
+//  DevilHouse
+//
+//  Created by Naito Seishiro on 2015/10/02.
+//  Copyright (c) 2015年 Itia. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+        // Do any additional setup after loading the view.
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
