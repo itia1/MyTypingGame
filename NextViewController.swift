7ff6d7a5fa1126521c8c0023ed5a0374a077c079
itia1 <itia0503@gmail.com>
2015-10-02 01:55:12 +0900

    //　ゲームスタート前のアニメーションを追加

--- NextViewController.swift
+++ NextViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class NextViewController: UIViewController {
-
+    var count = 2
+    @IBOutlet weak var countPicture: UIImageView!
+    var countPicture1 = UIImage(named:"number1.png")
+    var countPicture2 = UIImage(named:"number2.png")
+    var countPicture3 = UIImage(named:"number3.png")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+         NSTimer.scheduledTimerWithTimeInterval(0.7, target: self, selector: "countdown", userInfo: nil, repeats: true)
+        countPicture.image = countPicture3
     }
 
         // Do any additional setup after loading the view.
@@ -23,6 +29,25 @@ class NextViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func countdown(){
+        if(count==2){
+            countPicture.image = countPicture2
+        }else if(count==1){
+            countPicture.image = countPicture1
+        }else{
+            let storyboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let next:UIViewController = storyboard.instantiateViewControllerWithIdentifier("NextViewController") as! UIViewController
+            
+            next.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+                
+            self.presentViewController(next, animated: true, completion: nil)
+
+        }
+        
+        count--;
+        
+    }
 }
 
     /*
