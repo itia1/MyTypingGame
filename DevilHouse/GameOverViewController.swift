77829d4301484a2946ff59e6f4d5ca312f400021
itia1 <itia0503@gmail.com>
2015-10-08 22:23:28 +0900

    add 3false

---
+++ DevilHouse/GameOverViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameOverViewController.swift
+//  DevilHouse
+//
+//  Created by Naito Seishiro on 2015/10/08.
+//  Copyright © 2015年 Itia. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
